ba4b44a4ff799747e0d2dd12bbbf1f51114463ef
bdina1 <brian.n.dina@gmail.com>
2023-07-27 03:18:39 +0000

    tb fix

--- model/models.py
+++ model/models.py
@@ -29,6 +29,7 @@ class TB_object:
             'add_scalar': self.writer.add_scalar,
             'add_histogram': self.writer.add_histogram,
             'add_image': self.writer.add_image,
+            'add_weights': self.writer.add_image,
         }
         
     def __call__(self, prev_layer_output=None, layer_index=None):
@@ -36,42 +37,30 @@ class TB_object:
         operation = self.operations.get(operation_name)
         if operation is None:
             raise ValueError(f'Unknown operation: {operation_name}')
-
         if operation_name == "add_image":
             img_name = self.config[1]
-
-            # Check if output is a 4D tensor (batch_size, channels, height, width)
             if len(prev_layer_output.shape) == 4:
+                # 4D tensor - handle filter outputs
                 if prev_layer_output.shape[1] > 3:
-                    channel_images = []
+                    # Many filters - log each filter separately
                     for i in range(prev_layer_output.shape[1]):
-                        single_channel = prev_layer_output[:, i, :, :].unsqueeze(1)
-                        channel_images.append(single_channel)
-                        
-                    grid = torchvision.utils.make_grid(torch.cat(channel_images, dim=1), nrow=3)
-                    # Convert grid tensor to numpy array with data type uint8
-                    grid_np = (grid * 255).byte().permute(1, 2, 0).numpy()
+                        filter_img = prev_layer_output[:, i, :, :]
+                        filter_img = filter_img.unsqueeze(1)
+                        grid = torchvision.utils.make_grid(filter_img)
+                        operation(f'{img_name}_filter{i}', grid, 0)
 
-                    # Create PIL Image from numpy array
-                    image = Image.fromarray(grid_np)
+                # else:
+                #     # Few filters - stack them in grid
+                #     grid = prev_layer_output
+                #     grid = grid.permute(1, 0, 2, 3) 
+                #     grid = grid.reshape(grid.shape[0], -1, grid.shape[2])
+                #     operation(img_name, grid, 0)
 
-                    # Convert PIL Image back to tensor
-                    grid_tensor = torchvision.transforms.ToTensor()(image)
-
-                    operation(img_name, grid_tensor, 0)
                 else:
+                    # 3D tensor - add as image  
                     grid = torchvision.utils.make_grid(prev_layer_output)
                     operation(f'{img_name}_layer{layer_index}', grid, 0)
-            else:
-                raise ValueError(f'Cannot create image from layer output: {prev_layer_output.shape}')
-        # elif operation_name == "add_histogram":
-        #     hist_name = self.config[1]
-            
-        #     hist_data = prev_layer_output.detach().cpu().numpy()  # Convert tensor to numpy array
-        #     if hist_data[1] > 1:
-        #         pass
-        #     operation(hist_name, hist_data, bins='auto')  # Use 'auto' to determine the number of bins automatically
-            
+                
         else:
             # Now config should contain the parameters for the operation in the correct order
             operation(*self.config[1:], prev_layer_output)
